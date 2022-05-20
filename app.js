096a14b88081887a485750660d4c015d70d734ca
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-05-20 10:11:11 +0100

    Created get request for all recipes

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ import express from "express";
 
 import { html } from "./config.js";
 
+// import our router ✅
+
+import recipesRouter from "./router/recipes.js";
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
+
+// use our router ✅
+app.use(recipesRouter);
 
 /* DO NOT CHANGE THIS ROUTE - it serves our front-end */
 app.get("/", function (req, res) {
--- router/recipes.js
+++ router/recipes.js
@@ -1,3 +1,29 @@
 //Write your router code here!
 
-module.exports = recipesRouter;
+//import recipes array from recipes.js ✅
+import recipes from "../libs/recipes.js";
+
+// import express and store it in a variable called express ✅ 
+import express from "express";
+
+// create a router called router ✅ 
+const recipesRouter = express.Router();
+
+// Get all recipes
+// use our get method on the router
+    // path '/recipes'
+    // req res
+// create a responseObject with the correct properties and values
+// when receive a request at recipes path, respond with the responseObject
+// payload of all recipes✅
+
+recipesRouter.get("/recipes", function (req, res) {
+    const responseObject = {
+        success: true,
+        payload: recipes
+    };
+    res.json(responseObject);
+});
+
+// export our router using ES6 ✅
+export default recipesRouter;
