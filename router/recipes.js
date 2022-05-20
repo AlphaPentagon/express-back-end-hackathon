b345fdb303a834ccca2c4e8bee444d9abceb7bf9
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-05-20 12:08:12 +0100

    Created put method to replace recipe with matching ID

--- router/recipes.js
+++ router/recipes.js
@@ -76,5 +76,44 @@ recipesRouter.post("/", function (req, res) {
   res.json(responseObject);
 });
 
+// create a put method to replace a recipe with a specific id ✅
+// find the ID number specified in the request ✅
+// see if a recipe with the same ID exists in the array ✅
+// if so, then replace the object with the new object ✅
+// respond with response message and payload ✅
+// if the foor loop does not find a matching ID ✅
+// return some sort of error message ✅
+
+recipesRouter.put("/:id", (req, res) => {
+  // gets the value of the id property from the body of the request
+  let replacementRecipeId = req.body.id;
+  // gets the value of the body from the request
+  let replacementRecipe = req.body;
+
+  // for loop - loop over every element of the recipes array✅
+  for (let i = 0; i < recipes.length; i++) {
+    // if the id from the body request matches an id property in the array object✅
+    if (Number(replacementRecipeId) === recipes[i].id) {
+      // replace the recipe with the id for the new recipe ✅
+      recipes[i] = replacementRecipe;
+      // respond with the replacement recipe object
+      const responseObject = {
+        succes: true,
+        payload: replacementRecipe,
+      };
+      res.json(responseObject);
+      return;
+    }
+  }
+
+  // if the for loop does not find a match
+  // respond with a failure message
+  const responseObject = {
+    success: false,
+    message: `Cannot find recipe with id: ${replacementRecipeId}`,
+  };
+  res.json(responseObject);
+});
+
 // export our router using ES6 ✅
 export default recipesRouter;
